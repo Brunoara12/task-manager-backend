4b5a75f993f01810d358ecb5584c6d76592da84a
Bruno Desktop <brunoara12@gmail.com>
2020-05-19 02:04:34 -0400

    Set up MongoDB Database - Create

--- mongodb.js
+++ mongodb.js
@@ -13,5 +13,51 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
         return console.log('Unable to connect to database!')
     }
 
-    console.log('Connected correctly!')
+    const db = client.db(databaseName)
+
+    // db.collection('users').insertOne({
+    //     name: 'Bruno',
+    //     age: 27
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },{
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if(error){
+    //         return console.log('Unable to insert users')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'MongoDB Git',
+            completed: true
+        }, {
+            description: 'Challenge',
+            completed: false
+        }, {
+            description: 'Sleep',
+            completed: true
+        }
+    ], (error, result) => {
+        if(error){
+            return console.log('Unable to insert tasks')
+        }
+
+        console.log(result.ops)
+    })
 })
