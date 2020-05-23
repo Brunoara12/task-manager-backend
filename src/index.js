d28b23e019e00f2c36c95bdfe62cf51a816daae0
Brunoara12 <brunoara12@gmail.com>
2020-05-23 16:58:24 -0400

    Patch Endpoint for Task

--- src/index.js
+++ src/index.js
@@ -105,6 +105,29 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalid Updates!'})
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(error) {
+        res.status(500).send(error)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
