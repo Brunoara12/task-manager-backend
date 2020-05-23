2a6947fa3e1e7f9d3cfd448a271680360ead1033
Bruno Desktop <brunoara12@gmail.com>
2020-05-23 01:14:03 -0400

    Added Patch Endpoint for User with id

--- src/index.js
+++ src/index.js
@@ -8,68 +8,101 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((error) => {
+    } catch (error) {
         res.status(400).send(error)
-    })
+    }
+
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((error) => {
+    } catch (error) {
         res.status(500).send()
-    })
+    }
+
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id)
         if(!user) {
             return res.status(404).send()
         }
 
         res.send(user)
-    }).catch((error) => {
+    } catch(error) {
         res.status(500).send()
-    })
+    }
 })
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
+app.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
 
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((error) => {
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates!'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true})
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch(error) {
+        console.log(error)
         res.status(400).send(error)
-    })
+    }
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch(error) {
+        res.status(400).send(error)
+    }
+})
+
+app.get('/tasks', async (req, res) => {
+
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((error) => {
+    } catch(error) {
         res.status(500).send()
-    })
+    }
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if(!task) {
             return res.status(404).send()
         }
 
         res.send(task)
-    }).catch((error) => {
+    } catch(error) {
         res.status(500).send()
-    })
+    }
 })
 
 app.listen(port, () => {
