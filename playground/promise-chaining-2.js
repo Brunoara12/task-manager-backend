b3c5e53269ba48486e6eb761bd4eef22a92ef5bb
Bruno Desktop <brunoara12@gmail.com>
2020-05-23 01:14:03 -0400

    Added Patch Endpoint for User with id

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -10,3 +10,16 @@ Task.findByIdAndDelete('5ec7497f364ad953107507a9').then((task) => {
 }).catch((error) => {
     console.log(error)
 })
+
+const deleteTaskAndCountIncomplete = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const incompleteTasks = await Task.find({completed : false})
+
+    return incompleteTasks
+}
+
+deleteTaskAndCountIncomplete("5ec74975364ad953107507a8").then((tasks) => {
+    console.log(tasks, tasks.length)
+}).catch((error) => {
+    console.log(error)
+})
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5ec5f5ece5766d4b7c11e52c', { age: 1}).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1})
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5ec5f5ece5766d4b7c11e52c', { age: 1}).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age})
+    const count = await User.countDocuments( { age })
+
+    return count
+}
+
+updateAgeAndCount('5ec5f5ece5766d4b7c11e52c', 2).then((count) => {
+    console.log(count)
 }).catch((error) => {
     console.log(error)
 })
