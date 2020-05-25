7ed5997dc83819317c453d4f15f55418acd4faea
Bruno Desktop <brunoara12@gmail.com>
2020-05-25 12:05:04 -0400

    Added Routers for Mongoose Models

---
+++ src/routers/user-router.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+
+})
+
+router.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(500).send()
+    }
+
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch(error) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates!'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true})
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch(error) {
+        console.log(error)
+        res.status(400).send(error)
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try{
+        const user = await User.findByIdAndDelete(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch(error) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
