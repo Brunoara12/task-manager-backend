0c96f51bab0e6a02dec2e948c9bb2cc815fef19e
Bruno Desktop <brunoara12@gmail.com>
2020-06-12 23:55:49 -0400

    Authenticated Update and Delete User, Added User/Task Relationship

--- src/models/task.js
+++ src/models/task.js
@@ -9,6 +9,11 @@ const taskSchema = mongoose.Schema({
     completed: {
         type: Boolean,
         default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 })
 
