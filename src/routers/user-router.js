557c5b46982fd0412a83294f5253378aa87633ad
Bruno Desktop <brunoara12@gmail.com>
2020-06-12 23:55:49 -0400

    Authenticated Update and Delete User, Added User/Task Relationship

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -55,23 +55,7 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch(error) {
-        res.status(500).send()
-    }
-})
-
-router.patch('/users/:id', async (req, res) => {
-    const _id = req.params.id
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => {
@@ -83,33 +67,24 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
         updates.forEach((update) => {
-            user[update] = req.body[update]
+            req.user[update] = req.body[update]
         })
-        await user.save()
+        await req.user.save()
 
-        //const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true})
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        res.send(req.user)
     } catch(error) {
         console.log(error)
         res.status(400).send(error)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id
+router.delete('/users/me', auth, async (req, res) => {
 
     try{
-        const user = await User.findByIdAndDelete(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
+        await req.user.remove()
 
-        res.send(user)
+        res.send(req.user)
     } catch(error) {
         res.status(500).send()
     }
