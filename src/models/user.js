6adac71263f08e87255eed6b80fc649dd381e3f6
Bruno Desktop <brunoara12@gmail.com>
2020-06-15 23:21:11 -0400

    Added Sorting, Pagination, Filtering and Started File Uploads

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -48,6 +49,8 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 })
 
 userSchema.virtual('tasks', {
@@ -97,6 +100,12 @@ userSchema.pre('save', async function (next) {
         this.password = await bcrypt.hash(this.password, 8)
     }
 
+    next()
+})
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    await Task.deleteMany({ owner: this._id })
     next()
 })
 
