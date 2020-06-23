6ff597730338402eb9d30d82483bbf3315617d23
Brunoara12 <brunoara12@gmail.com>
2020-06-23 17:09:48 -0400

    Task File Uploads Complete and Started Send Emails

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 require('./db/mongoose')
-
 const userRouter = require('./routers/user-router')
 const taskRouter = require('./routers/task-router')
 
@@ -8,20 +7,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images'
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-})
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
-
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
